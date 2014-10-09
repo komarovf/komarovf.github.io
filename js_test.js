afa8cf541b2b2c092688095568a32824ae8a44fc
Sergey <s-e-r-g-e-y@ukr.net>
2014-10-09 12:25:44 +0300

    Update js_test.js

--- js_test.js
+++ js_test.js
@@ -1,16 +1,28 @@
 /*
 Tests for functions from gaussian_elimination.js.
-I don't know how to test func like insert_matrix(A, element) /: 
 
+P.S. Guys! I've never written tests for js before :(
 */
 
-
+//check_value()
 QUnit.test('check_value(input field)', function ( assert ) {
     assert.equal(check_value(''), true, 'Empty field is OK');
-    assert.equal(check_value('   '), false, 'Only spaces not accepted');
+	assert.equal(check_value('123'), true, 'Numbers - OK');
+	assert.equal(check_value('1e-2'), true, 'Numbers - OK');
+	assert.equal(check_value('1.256'), true, 'Numbers - OK');
     assert.equal(check_value('  5'), true, 'Spaces before number - ok');
     assert.equal(check_value('5 '), true, 'Spaces after number - ok');
     assert.equal(check_value('  5  '), true, 'Spaces around number - ok too');
+    assert.equal(check_value('   '), false, 'Only spaces not accepted');
     assert.equal(check_value('5 5'), false, 'Number separated by spaces - not accepted');
     assert.equal(check_value('x5'), false, 'chars not accepted');
+	assert.equal(check_value('infinity'), false, 'infinity not accepted');
+});
+
+//insert_matrix()
+QUnit.test('insert_matrix(matrix, DOM element)', function ( assert ) {
+    A_test = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]];
+	elem = document.createElement('div');
+	insert_matrix(A_test, elem);
+	assert.equal(elem.firstChild.rows.length == 4, 'Correct number of rows');
 });
